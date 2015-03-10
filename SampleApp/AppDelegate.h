d83432fde7615ac87d087522d9b88526290ae99b
Prachi Waghmare <pracwagh@in.ibm.com>
2015-03-10 13:49:16 +0530

    Initial Commit

---
+++ SampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleApp
+//
+//  Created by Prachi on 3/10/15.
+//  Copyright (c) 2015 Prachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by Prachi on 3/10/15.
+//  Copyright (c) 2015 Prachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
